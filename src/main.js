046a39b91a41dbd3f1af36c489762dffa31a7cdf
linzhen <273487984@qq.com>
2018-04-24 15:07:32 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+import routes from './route.js'
+
+const router = new VueRouter({
+  routes
+})
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+new Vue({
+  render: (h) => h(App),
+  router
+}).$mount(root)
